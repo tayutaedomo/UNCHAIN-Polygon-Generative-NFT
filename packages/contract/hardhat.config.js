2efce49860b50a325782a46fb363a90da94afae4
tayutaedomo <tayutaedomo@gmail.com>
2023-08-09 23:18:13 +0900

    Section 2 Lesson 2 is completed

--- packages/contract/hardhat.config.js
+++ packages/contract/hardhat.config.js
@@ -1,6 +1,15 @@
 require("@nomicfoundation/hardhat-toolbox");
 
+const { API_URL, PRIVATE_KEY } = process.env;
+
 /** @type import('hardhat/config').HardhatUserConfig */
 module.exports = {
   solidity: "0.8.18",
+  defaultNetwork: "sepolia",
+  networks: {
+    sepolia: {
+      url: API_URL,
+      accounts: [PRIVATE_KEY],
+    },
+  },
 };
